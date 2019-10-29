50d626c9ccf9b9d596f9b694fcad1d7b154c0a19
Austin Appel <austinappel@ufl.edu>
2019-10-29 17:46:02 -0400

    added filtering

--- src/components/BuildingList.js
+++ src/components/BuildingList.js
@@ -3,16 +3,23 @@ import React from 'react';
 class BuilingList extends React.Component {
 	render() {
 		//console.log('This is my directory file', this.props.data);
+		
 		const { data } = this.props;
+		const filterText = this.props.filterText; 
 
-		const buildingList = data.map(directory => {
-			return (
-				<tr key={directory.id}>
-					<td>{directory.code} </td>
-					<td> {directory.name} </td>
-				</tr>
-			);
-		});
+		const buildingList = data
+			.filter(directory => {
+				console.log('Names: ', directory.name.toLowerCase())
+				return directory.name.toLowerCase().indexOf(filterText.toLowerCase()) >= 0
+			})
+			.map(directory => {
+				return (
+					<tr key={directory.id}>
+						<td>{directory.code} </td>
+						<td> {directory.name} </td>
+					</tr>
+				);
+			});
 
 		return <div>{buildingList}</div>;
 	}
